cdd1b28f33c946bed48f48769c48700e75546704
shabareesha <arjunspy491@gmail.com>
2026-06-01 16:13:25 +0530

    improved command validation(command whitelist)

---
+++ packages/config/allowed-actions.js
@@ -0,0 +1,8 @@
+export const ALLOWED_ACTIONS = [
+  "open_app",
+  "close_app",
+  "open_url",
+  "google_search",
+  "youtube_search",
+  "play_youtube",
+];
--- packages/schemas/command.schema.js
+++ packages/schemas/command.schema.js
@@ -1,6 +1,7 @@
 import { z } from "zod";
+import { ALLOWED_ACTIONS } from "../config/allowed-actions.js";
 
 export const CommandSchema = z.object({
-  action: z.string(),
+  action: z.enum(ALLOWED_ACTIONS),
   params: z.object({}).passthrough(),
 });
